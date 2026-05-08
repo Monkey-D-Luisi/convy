105da61d3b4a1be0123567ce500a92d88bf4c079
Luis Aitor Bahillo Alonso <Luisaitorbahilloalonso@gmail.com>
2026-05-08 13:51:14 +0200

    chore: bump android staging test version

--- mobile/androidApp/build.gradle.kts
+++ mobile/androidApp/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.monkeydluisi.convy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.1.9"
+        versionCode = 13
+        versionName = "0.1.10"
     }
 
     signingConfigs {
