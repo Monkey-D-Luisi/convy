3f745d3a32b33f5ad28dc448ffaee5c8e665aafa
Luis Aitor Bahillo Alonso <Luisaitorbahilloalonso@gmail.com>
2026-05-30 15:58:49 +0200

    chore: bump android release version

--- mobile/androidApp/build.gradle.kts
+++ mobile/androidApp/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.monkeydluisi.convy"
         minSdk = 31
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.1.22"
+        versionCode = 26
+        versionName = "0.1.23"
     }
 
     signingConfigs {
